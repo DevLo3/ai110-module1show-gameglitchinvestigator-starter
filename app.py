56081efee0b02b97a7dc62bd0e61f981efec0e7b
DevLo3 <98679687+DevLo3@users.noreply.github.com>
2026-03-17 22:20:26 -0500

    Ensure the start of a new game resets all stata variables

--- app.py
+++ app.py
@@ -70,10 +70,11 @@ with col2:
 with col3:
     show_hint = st.checkbox("Show hint", value=True)
 
-# FIXME: below logic fails to also reset the "history" and "status" session state variables when a user requests a new game
 if new_game:
     st.session_state.attempts = 0
     st.session_state.secret = random.randint(1, 100)
+    st.session_state.history = []
+    st.session_state.status = "playing"
     st.success("New game started.")
     st.rerun()
 
